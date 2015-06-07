13ba981d9b336376d9341f7cd62be1a94a5b21c9
Pengxiang <jpxx@umich.edu>
2015-06-07 15:09:27 -0500

    Corrected a spelling mistake

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 	def create
 		super do |resource|
-			if param[:plan]
+			if params[:plan]
 				resource.plan_id = params[:plan]
 				if resource.plan_id == 2
 					resource.save_with_payment
